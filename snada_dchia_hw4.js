05b904c795a2005f5d541f9e9fb56bd3b98f0fa9
davinchia <davinchia@outlook.com>
2015-10-22 18:20:18 -0400

    Cleaned up code

--- snada_dchia_hw4.js
+++ snada_dchia_hw4.js
@@ -25,95 +25,131 @@ var getJson = function(url){
     return promise;
 };
 
-var displayData = function(data){
-    var list = document.getElementById('movie-list');
-    var template = "<div class='shortView'> <img src=http://image.tmdb.org/t/p/w92/#poster_path#></img> <p class='title header'> #title# (#release_date#)</p> <p class='rating header'> Average Vote: #vote_average#</p></div>";
-    template +=     "<div class='detailView hidden'>";
-    template +=            "<img src='http://image.tmdb.org/t/p/w342/#poster_path#'> </img>"
-    template +=            "<div class = 'details'>"
-    template +=                 "<h1 class='title'> #original_title# (#release_date#)</h1> "
-    template +=                 "<ul>"
-    template +=                     "<li> Release Date: #full_release_date#</li>"
-    template +=                     "<li> Vote Average: #vote_average#</li>"
-    template +=                     "<li> Number of Votes: #vote_count#</li>"
-    template +=                     "<li> Summary: #overview#</li>"
-    template +=                 "</ul>"
-    template +=            "</div>"
-    template +=     "</div>"
+/*
+Sets up and returns the array of movies which we display on the DOM 
+@param selectedMovies - hashTable that tells us whether a movie is currently expanded on the screen
+@param data - JSON object containing movie info in its original format
+*/
+var setUp = function( data, isSelected){
+    var moviesArray = [];
+    var template = "<div class='viewContainer'>";
+    template +=         "<div class='shortView'> <img src=http://image.tmdb.org/t/p/w92/#poster_path#></img> <p class='title header'> #title# (#release_date#)</p> <p class='rating header'> Average Vote: #vote_average#</p></div>";
+    template +=            "<div class='detailView hidden'>";
+    template +=                 "<img src='http://image.tmdb.org/t/p/w342/#poster_path#'> </img>";
+    template +=                 "<div class = 'details'>";
+    template +=                 "<h1 class='title'> #original_title# (#release_date#)</h1> ";
+    template +=                 "<ul>";
+    template +=                     "<li> Release Date: #full_release_date#</li>";
+    template +=                     "<li> Vote Average: #vote_average#</li>";
+    template +=                     "<li> Number of Votes: #vote_count#</li>";
+    template +=                     "<li> Summary: #overview#</li>";
+    template +=                 "</ul>";
+    template +=                 "</div>";
+    template +=         "</div>";
+    template +=     "</div>";
+
     Object.keys(data).forEach(function(key){
+        var domElementWrapper = [];
         var item = document.createElement("li");
 
         var content = template.replace(/#[^#]*#/g, function(substring) {
             var property = substring.slice(1, -1);
+            var answer;
             if (property === "release_date") {
-                return data[key][property].slice(0,4);
+                answer = data[key][property].slice(0,4);
             } else if(property === "full_release_date"){
-                return data[key]["release_date"];
+                answer = data[key]["release_date"];
             }
-            else { 
-                return data[key][property];
+            else { //original_title
+                answer =  data[key][property];
             }
+
+            //asign the properties to the javascript item so that it can be easily sorted
+            if (property === "title" || property === "release_date" ||  property === "vote_average") {
+                if (property === "release_date") {
+                    domElementWrapper[property] = data[key][property].slice(0,4);
+                } else {
+                    domElementWrapper[property] = data[key][property];
+                }
+            }
+
+            return answer;
         });
         item.innerHTML = content;
-        list.appendChild(item);
+        // list.appendChild(item);
 
         item.addEventListener("click", function() {
             
-            // Contract view if selected twice
+            //Contract detail view and display short view
             if (item.getElementsByClassName("detailView")[0].className === "detailView selected") {
                 item.getElementsByClassName("detailView")[0].className = "detailView hidden";
                 item.getElementsByClassName("shortView")[0].className = "shortView";
+                isSelected[domElementWrapper.title + domElementWrapper.release_date] = undefined;
             }
-            
+            // Expand detail view if contracted
             else {
-                // var selectedDetail = list.getElementsByClassName("selected detailView");
-
-                // for (var i = 0; i < selectedDetail.length; i++){
-                //     selectedDetail[i].className = "detailView hidden";
-                // }
-
-                // var hiddenShorts = list.getElementsByClassName("hidden shortView");
-                // for (var i = 0; i < hiddenShorts.length; i++){
-                //     hiddenShorts[i].className = 'shortView';
-                // }
-
                 item.getElementsByClassName("detailView")[0].className = "detailView selected" ;
                 item.getElementsByClassName("shortView")[0].className = "shortView hidden ";
+                isSelected[domElementWrapper.title + domElementWrapper.release_date] = 1;   
             }
         });
+
+        domElementWrapper.item = item;
+        moviesArray.push(domElementWrapper);
     });
+
+    return moviesArray;
 };
+
+//Display the elements of moviesArray on the page
+function displayData(moviesArray, isSelected) {
+    var movie_list_dom = document.getElementById("movie-list"); 
+    movie_list_dom.innerHTML = "";   //erase whatever is currently displayed 
+
+    //display the new list
+    moviesArray.forEach(function(movieInfo) {
+
+        if (isSelected[movieInfo.title + movieInfo.release_date]) {
+            movieInfo.item.getElementsByClassName("detailView")[0].className = "detailView selected" ;
+            movieInfo.item.getElementsByClassName("shortView")[0].className = "shortView hidden ";
+        }
+        movie_list_dom.appendChild(movieInfo.item);
+    });
+}
 
 getJson('http://localhost:8000/movies.json')
 .then(function(response){
     var selectList = document.getElementById("select");
     var orderList = document.getElementById("select-order");
-
+    var selectedMovies = [];    //keeps track of what movies are opened
+    var moviesArray = setUp(response["movies"], selectedMovies); //keeps track of all the movies objects so that we do not need to destroy them 
+    
     var updateList =  function(){
-        sortByProperty(response["movies"], selectList.value, orderList.value);  
-        document.getElementById("movie-list").innerHTML = "";
-        displayData(response["movies"]);
+        sortByProperty(moviesArray, selectList.value, orderList.value);  
+        displayData(moviesArray, selectedMovies);
     };
+
+    displayData(moviesArray, selectedMovies);
     selectList.onchange = updateList;
     orderList.onchange = updateList;
-
-    updateList();   
 })
 .catch(function(event){
     console.log(event);
 });
 
-var sortByProperty = function(list, property, order) {
-    // 1 for ascending means true
-    // -1 means false
+
+var sortByProperty = function(moviesArray, property, order) {
+
+    // // 1 for ascending means true
+    // // -1 means false
     var ascending;
-    if(order == "ascending"){
+    if (order == "ascending"){
         ascending = 1;
     }else{
         ascending = -1;
     }
 
-    list.sort(function(a,b){
+    moviesArray.sort(function(a,b){
         if (a[property] > b[property])
             return 1 * ascending;
         if(a[property] === b[property])
